53a6bb4612196d77b3d40e3215ba40fdce1704a0
kouya takahashi <3kamarin4skaya6@gmail.com>
2022-08-22 21:05:39 +0900

    add

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 csvファイル読み込み、なければ自前のサンプルデータ読み込み
 データ表示
 番組名、MC絞り込み機能
-ランダムに表示
-
 """
 import pandas as pd
+FILE_PATH = "https://drive.google.com/file/d/1rmZoK6ZCrLxiGIL58aIKlvP4vDLMr2Ip/view?usp=sharing"
+# KEY =
